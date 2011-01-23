12e0a05040eef512dc3920d1e3e014923861bf34
merlin <merlin@flafla.fr>
2011-01-23 18:12:01 +0000

    Add cameras to robot.

--- UrbiDroid/urbi/fr/flafla/android/urbi/robot/Robot.java
+++ UrbiDroid/urbi/fr/flafla/android/urbi/robot/Robot.java
@@ -4,10 +4,9 @@
 package fr.flafla.android.urbi.robot;
 
 
-import android.graphics.Bitmap;
-import android.util.Log;
 import fr.flafla.android.urbi.UClient;
 import fr.flafla.android.urbi.control.Axes;
+import fr.flafla.android.urbi.control.Camera;
 
 /**
  * Classe abstraite d'accès aux fonctionnalité d'un robot géré par urbi
@@ -16,11 +15,10 @@ import fr.flafla.android.urbi.control.Axes;
  *
  */
 public abstract class Robot extends UClient {
-	public static interface ImageHandler {
-		public void handle(Bitmap bitmap);
-	}
-	
+
 	public final Axes[] axes;
+
+	public final Camera[] cameras;
 
 	/**
 	 * Le robot géré par le système (un seul à la fois)
@@ -31,44 +29,13 @@ public abstract class Robot extends UClient {
 	
 	protected boolean stop = false;
 	
-	protected Robot(String ip, int port, Axes... axes) {
+	protected Robot(String ip, int port, Axes[] axes, Camera... cameras) {
 		this.axes = axes;
+		this.cameras = cameras;
 	}
 
 	public abstract void go(int trackL, int trackR);
 	
 	
-	protected abstract void initCamera();
 	
-	public void acquire(final ImageHandler handler) {
-		initCamera();
-		if (thread == null) {
-			thread = new Thread() {
-				public void run() {
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						Log.e(getClass().getSimpleName(), "Thread error", e);
-					}
-					while (!stop) {
-						try {
-							Thread.sleep(100);
-						} catch (InterruptedException e) {
-							Log.e(getClass().getSimpleName(), "Thread error", e);
-						}
-						handler.handle(getImage());
-					}
-				}
-			};
-
-		}
-		
-		thread.start();
-	}
-	
-	public void stopAcquire() {
-		stop = true;
-	}
-	
-	protected abstract Bitmap getImage();
 }
