8ddcf9349b0c790a692f4f31da7b3e9a891842f8
merlin <merlin@flafla.fr>
2011-01-23 17:21:58 +0000

    Axis model.

---
+++ UrbiDroid/urbi/fr/flafla/android/urbi/control/Axes.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package fr.flafla.android.urbi.control;
+
+/**
+ * 
+ * 
+ * @author merlin
+ * 
+ */
+public class Axes {
+	public static class Axis {
+		/** Axe min value */
+		public final int minValue;
+		/** Axe max value */
+		public final int maxValue;
+
+		/**
+		 * Construct an axis with min and max values
+		 * @param minValue
+		 * @param maxValue
+		 */
+		public Axis(int minValue, int maxValue) {
+			this.minValue = minValue;
+			this.maxValue = maxValue;
+		}
+	}
+
+	/** X axis (nullable) */
+	public final Axis x;
+	/** Y axis (nullable) */
+	public final Axis y;
+
+	/**
+	 * Constructor
+	 * @param x
+	 * @param y
+	 */
+	public Axes(Axis x, Axis y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
