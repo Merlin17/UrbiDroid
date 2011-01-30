bdab26641881a10bff6f63cc3ef162d0947ba199
merlin <merlin@flafla.fr>
2011-01-30 21:52:21 +0000

    Test spykee urbi implementation.

--- UrbiTest/src/fr/flafla/android/urbi/test/UrbiTest.java
+++ UrbiTest/src/fr/flafla/android/urbi/test/UrbiTest.java
@@ -1,19 +1,25 @@
 package fr.flafla.android.urbi.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import junit.framework.TestCase;
 import fr.flafla.android.urbi.UCallback;
 import fr.flafla.android.urbi.UClient;
 import fr.flafla.android.urbi.UMessage;
+import fr.flafla.android.urbi.control.Camera.ImageHandler;
+import fr.flafla.android.urbi.robot.Spykee;
 
 public class UrbiTest extends TestCase {
-	private UClient client;
-
 	@Override
 	protected void setUp() throws Exception {
-		client = new UClient("localhost", UClient.PORT);
 	}
 
 	public void test1() throws Exception {
+		UClient client = new UClient("localhost", UClient.PORT);
 		client.addCallback("uimg", new UCallback() {
 			@Override
 			public boolean handle(UMessage msg) {
@@ -32,10 +38,58 @@ public class UrbiTest extends TestCase {
 		return;
 	}
 
+	public void testSpykee() throws Exception {
+		// Directory to test images
+		final File dir = new File("/tmp/test/");
+		dir.mkdirs();
+
+		// Init
+		Spykee robot = new Spykee("192.168.1.14", UClient.PORT);
+		robot.getCameras()[0].addHandler(new ImageHandler() {
+			long last = System.currentTimeMillis();
+			int img = 1;
+			@Override
+			public void handle(InputStream bitmap) {
+				System.out.println("New bitmap from spykee camera");
+				try {
+					FileOutputStream out = new FileOutputStream(new File(dir, "img" + (img++) + ".jpg"));
+					byte[] b = new byte[1024];
+					while ((bitmap.read(b)) > 0)
+						out.write(b);
+					out.close();
+					System.out.println("time : " + (System.currentTimeMillis() - last));
+					last = System.currentTimeMillis();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		});
+		robot.getCameras()[0].start(200);
+		
+
+		// // Just wait images
+		// Thread.sleep(5000);
+
+		robot.getAxes()[0].y.value = 10;
+		robot.getAxes()[1].y.value = 10;
+		robot.move();
+
+		Thread.sleep(1000);
+
+		robot.getAxes()[0].y.value = 0;
+		robot.getAxes()[1].y.value = 0;
+		robot.move();
+
+	}
+
 	public static void main(String[] args) throws Exception {
 		UrbiTest test = new UrbiTest();
-		test.setUp();
-		test.test1();
+		// test.setUp();
+		// test.test1();
+		test.testSpykee();
 
 	}
 
