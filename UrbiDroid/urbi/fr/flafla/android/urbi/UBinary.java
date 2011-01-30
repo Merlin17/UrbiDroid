43915567e8426509cc2b663e95b314c2b8df921c
merlin <merlin@flafla.fr>
2011-01-30 21:09:26 +0000

    Add length to UBinary message.

--- UrbiDroid/urbi/fr/flafla/android/urbi/UBinary.java
+++ UrbiDroid/urbi/fr/flafla/android/urbi/UBinary.java
@@ -9,9 +9,11 @@ package fr.flafla.android.urbi;
  */
 public class UBinary extends UMessage {
 	public final byte[] array;
+	public final int length;
 
-	public UBinary(String tag, String time, String msg, byte[] array) {
+	public UBinary(String tag, String time, String msg, int length, byte[] array) {
 		super(tag, time, msg);
+		this.length = length;
 		this.array = array;
 	}
 
